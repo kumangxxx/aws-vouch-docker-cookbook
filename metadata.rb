6b1a922d929f3595308e27f0158e57b8700f8e53
Rahadian Ahmad <kumangxxx@gmail.com>
2018-05-07 19:28:28 +0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,7 @@
 name "aws-vouch-docker-cookbook"
 
+source_url 'https://github.com/NOX73/chef-golang' if respond_to?(:source_url)
+issues_url 'https://github.com/NOX73/chef-golang/issues' if respond_to?(:issues_url)
+
 depends "apt"
 depends "docker"
