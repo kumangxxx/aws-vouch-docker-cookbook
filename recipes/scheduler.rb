940848c952c84f5516e28141ea25015b54eb719b
Rahadiann <rahadian@vouch.sg>
2018-05-07 22:50:46 +0700

    test recipe 2

--- recipes/scheduler.rb
+++ recipes/scheduler.rb
@@ -1,3 +1,22 @@
-execute "Pull scheduler" do 
-    command "docker stop schedule1 && docker rm schedule1"
+bash "docker-cleanup" do
+    user "root"
+    returns [0, 1]
+    code <<-EOH
+    if docker ps | grep scheduler1;
+    then
+        docker stop scheduler1
+        sleep 3
+        docker rm scheduler1
+    fi
+    if docker ps -a | grep scheduler1;
+    then
+        docker rm scheduler1
+    fi 
+    if docker images | grep vouchdocker;
+    then
+        docker rmi $(sudo docker images | grep -m 1 vouchdocker | awk {'print $3'})
+    fi
+    EOH
 end
+
+Chef::Log.info('Docker cleaned')
