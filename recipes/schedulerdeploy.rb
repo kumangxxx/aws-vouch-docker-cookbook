e9fba4f1d402f128a9c98aa6832f64bd088bc268
Rahadiann <rahadian@vouch.sg>
2018-05-08 17:23:39 +0700

    fix deploy script

--- recipes/schedulerdeploy.rb
+++ recipes/schedulerdeploy.rb
@@ -3,20 +3,15 @@ bash "docker-cleanup" do
     user "root"
     returns [0, 1, 2]
     code <<-EOH
-        if docker ps | grep scheduler1;
-        then
-            docker stop scheduler1
-            sleep 3
-            docker rm scheduler1
-        fi
-        if docker ps -a | grep scheduler1;
-        then
-            docker rm scheduler1
-        fi 
-        if docker images | grep vouchdocker;
-        then
-            docker rmi $(sudo docker images | grep -m 1 vouchdocker | awk {'print $3'})
-        fi
+        docker stop scheduler1 && docker rm scheduler1
+    EOH
+end
+
+bash "docker-rmi" do
+    user "root"
+    returns [0, 1, 2]
+    code <<-EOH
+        docker rmi $(sudo docker images | grep -m 1 vouchdocker | awk {'print $3'})
     EOH
 end
 
