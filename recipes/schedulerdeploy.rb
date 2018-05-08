28ad14c3c77731c4e964c269a53a36bc3bc9c6a2
Rahadiann <rahadian@vouch.sg>
2018-05-08 17:26:43 +0700

    fix deploy script

--- recipes/schedulerdeploy.rb
+++ recipes/schedulerdeploy.rb
@@ -1,7 +1,7 @@
 Chef::Log.info("***************** Deploying ***************")
 bash "docker-cleanup" do
     user "root"
-    returns [0, 1, 2]
+    returns [0, 1, 2, 125]
     code <<-EOH
         docker stop scheduler1 && docker rm scheduler1
     EOH
@@ -9,7 +9,7 @@ end
 
 bash "docker-rmi" do
     user "root"
-    returns [0, 1, 2]
+    returns [0, 1, 2, 125]
     code <<-EOH
         docker rmi $(sudo docker images | grep -m 1 vouchdocker | awk {'print $3'})
     EOH
@@ -19,7 +19,7 @@ Chef::Log.info('*********** Docker cleaned, pulling and running ****************
 
 bash "docker-login" do
     user "root"
-    returns [0, 1, 2]
+    returns [0, 1, 2, 125]
     code <<-EOH
         docker login -u vouchdocker -p 123dockerhub456
     EOH
@@ -27,7 +27,7 @@ end
 
 bash "docker-run" do
     user "root"
-    returns [0, 1, 2]
+    returns [0, 1, 2, 125]
     code <<-EOH
         docker run --name schedule1 -p 6001:8001 -e ENV=_PRODUCTION -e SERVICE_NAME=scheduler_1 -e REDIS_HOST=scheduler-redis.2kk9v8.ng.0001.apse1.cache.amazonaws.com:6379 --restart always vouchdocker/schedule-service:0.2.12
     EOH
