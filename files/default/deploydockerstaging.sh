73056135167e9eca2c981231f810e8a7041ba7cc
Rahadian <rahadian@vouch.sg>
2019-10-25 11:09:55 +0700

    change docker tag for staging:

--- files/default/deploydockerstaging.sh
+++ files/default/deploydockerstaging.sh
@@ -2,5 +2,5 @@ echo "-- deploying --"
 docker stop schedule1 && docker rm schedule1
 docker login -u vouchdocker -p 123dockerhub456
 docker rmi $(docker images -q)
-docker run -d --name schedule1 -p 6001:8001 -e ENV=_STAGING -e SERVICE_NAME=scheduler_1 -e REDIS_HOST=scheduler-redis.2kk9v8.ng.0001.apse1.cache.amazonaws.com:6379 --restart always vouchdocker/schedule-service:latest
+docker run -d --name schedule1 -p 6001:8001 -e ENV=_STAGING -e SERVICE_NAME=scheduler_1 -e REDIS_HOST=scheduler-redis.2kk9v8.ng.0001.apse1.cache.amazonaws.com:6379 --restart always vouchdocker/schedule-service:staging
 echo "-- end deploy --"
